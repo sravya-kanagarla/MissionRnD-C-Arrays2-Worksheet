8916c55481a540bdbad4861c275bc1b7f1c5eefc
sravya-kanagarla <sravya-kanagarla@missionrnd.com>
2016-01-14 19:53:40 +0530

    Submit Commit

--- src/countGreaterNumbers.cpp
+++ src/countGreaterNumbers.cpp
@@ -14,12 +14,75 @@ ERROR CASES: Return NULL for invalid inputs.
 NOTES:
 */
 
+#include <stdio.h>
+
 struct transaction {
 	int amount;
 	char date[11];
 	char description[20];
 };
 
+int compareDate(char date1[], char *date2) {
+	int d1 = (date1[0] - '0') * 10 + (date1[1] - '0');
+	int d2 = (date2[0] - '0') * 10 + (date2[1] - '0');
+	int month1 = (date1[3] - '0') * 10 + (date1[4] - '0');
+	int month2 = (date2[3] - '0') * 10 + (date2[4] - '0');
+	int year1 = (date1[6] - '0') * 1000 + (date1[7] - '0') * 100 + (date1[8] - '0') * 10 + (date1[9] - '0');
+	int year2 = (date2[6] - '0') * 1000 + (date2[7] - '0') * 100 + (date2[8] - '0') * 10 + (date2[9] - '0');
+	if (year1 < year2) {
+		return 1;
+	}
+	else if (year1 > year2) {
+		return 2;
+	}
+	else {
+		if (month1 < month2) {
+			return 1;
+		}
+		else if (month1 > month2) {
+			return 2;
+		}
+		else {
+			if (d1 < d2) {
+				return 1;
+			}
+			else if (d1 > d2) {
+				return 2;
+			}
+			else {
+				return 0;
+			}
+		}
+	}
+
+}
+
 int countGreaterNumbers(struct transaction *Arr, int len, char *date) {
+	int start = 0;
+	int end = len - 1;
+	int compare;
+	while (start < end) {
+		compare = compareDate((Arr + start)->date, date);
+		if (compare == 1)
+			start += 1;
+		else if (compare == 0) {
+			start += 1;
+			break;
+		}
+		compare = compareDate((Arr + end)->date, date);
+		if (compare == 2)
+			end -= 1;
+		else if (compare == 0)
+			return len - end - 1;
+	}
+	while (start < end) {
+		compare = compareDate((Arr + start)->date, date);
+		if (compare == 0)
+			start += 1;
+		else
+			return len - start;
+	}
+	if (start == end)
+		return len - end - 1;
 	return -1;
 }
--- src/findSingleOccurenceNumber.cpp
+++ src/findSingleOccurenceNumber.cpp
@@ -13,6 +13,49 @@ ERROR CASES: Return -1 for invalid inputs.
 NOTES:
 */
 
+#include <stdlib.h>
+
+struct node {
+	int value;
+	int count;
+	struct node *next;
+};
+
 int findSingleOccurenceNumber(int *A, int len) {
+	if (A == NULL)
+		return -1;
+	int i;
+	struct node *heads[10];
+	struct node *iter;
+	for (i = 0; i < 10; i++) {
+		heads[i] = (struct node *) malloc(sizeof(struct node));
+		heads[i]->next = NULL;
+		heads[i]->value = -1;
+		heads[i]->count = 0;
+	}
+	for (i = 0; i < len; i++) {
+		iter = heads[A[i] % 10];
+		while (iter->next != NULL && iter->value != A[i])
+			iter = iter->next;
+		if (iter->value == A[i])
+			iter->count += 1;
+		else {
+			if (iter->value != -1) {
+				iter->next = (struct node *) malloc(sizeof(struct node));
+				iter = iter->next;
+				iter->next = NULL;
+			}
+			iter->value = A[i];
+			iter->count = 1;
+		}
+	}
+	for (i = 0; i < 10; i++) {
+		iter = heads[i];
+		while (iter != NULL && iter->count != 1) {
+			iter = iter->next;
+		}
+		if (iter != NULL && iter->count == 1)
+			return iter->value;
+	}
 	return -1;
 }
